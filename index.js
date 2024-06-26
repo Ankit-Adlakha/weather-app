35f7cd5b8fbf1e873b76de5e6073e94d14ea1950
Ankit <92292251+Ankit-Adlakha@users.noreply.github.com>
2024-06-26 13:27:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const userInfoContainer = document.querySelector(".user-info-container");
 //initially vairables need????
 
 let oldTab = userTab;
-const API_KEY = "d1845658f92b31c64bd94f06f7188c9c";
+const API_KEY = "96d4ca65c49492cfd7c19ae763465f34";
 oldTab.classList.add("current-tab");
 getfromSessionStorage();
 
